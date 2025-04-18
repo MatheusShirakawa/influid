b3a3f2676665834c9436f1b72b92ee650e37381d
MatheusShirakawa <matheus_shirakawa@outlook.com>
2025-04-18 13:22:25 -0300

    :bug: fix errors with translations and wrong structure, and setting up the header texts

--- src/app/[locale]/components/header/index.tsx
+++ src/app/[locale]/components/header/index.tsx
@@ -1,17 +1,17 @@
-
-
+import { useTranslations } from 'next-intl'
 
 export default function Header(){
+	const t = useTranslations('Header')
+
 	return(
 		<header className="header">
 			<div className="container">
 				<div className="header-content">
 					<ul>
-						<li>about us.</li>
-						<li>our projects.</li>
-						<li>our services.</li>
-						<li>contact us.</li>
-						<li>our projects.</li>
+						<li>{t('about')}</li>
+						<li>{t('projects')}</li>
+						<li>{t('services')}</li>
+						<li>{t('contact')}</li>
 					</ul>
 				</div>
 			</div>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,27 @@
-import { useTranslations } from "next-intl";
+import { Locale, useTranslations } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
+import { use } from 'react';
+import Header from './components/header';
 
-export default function Home() {
-	const t = useTranslations("Home");
+type Props = {
+	params: Promise<{ locale: Locale }>;
+}
+
+export default function Home({ params }: Props) {
+	const { locale } = use(params);
+
+	// Enable static rendering
+	setRequestLocale(locale);
+
+	const t = useTranslations('Home')
+
 	return (
 		<div className="bg-gray-800">
 			<main>
+				<Header/>
 				{t("title")}
 				{t("description")}
 			</main>
 		</div>
-	);
+	)
 }
