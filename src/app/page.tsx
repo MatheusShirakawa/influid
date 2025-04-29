3202f79b33d953745e93486f40bb0bc680c8233e
MatheusShirakawa <matheus_shirakawa@outlook.com>
2025-04-29 20:06:44 -0300

    :bug: fix error locale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import { redirect } from 'next/navigation'
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
-	const lang = navigator.language
+	redirect('/en')
+	// const lang = navigator.language
 
-	if(lang === 'pt-BR' || lang === 'pt') {
-		redirect('/pt')
-	}else{
-		redirect('/en')
-	}
+	// if(lang === 'pt-BR' || lang === 'pt') {
+	// 	redirect('/pt')
+	// }else{
+	// 	redirect('/en')
+	// }
 }
