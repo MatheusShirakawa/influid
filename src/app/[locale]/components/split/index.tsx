845062df1be6514d367412a907af49b8b6e739fd
MatheusShirakawa <matheus_shirakawa@outlook.com>
2025-05-05 14:45:24 -0300

    :bug: fix little issue with css in split block

--- src/app/[locale]/components/split/index.tsx
+++ src/app/[locale]/components/split/index.tsx
@@ -8,62 +8,64 @@ export default function Split() {
 	const t = useTranslations('Split')
 	return(
 		<div id="split" className='split'>
-			<div className='content-split'>
-				<div className='left'>
-					<div className='line-numbers'>
-						<div className='line'>
-							<div className='content-number'>
-								<span className='plus text-[#00FF99] text-2xl mt-4'>+</span>
-								<span
-									className='counter number text-[54px] text-white'
-									style={{ '--from':6, '--to': 10, '--time': '6s' } as React.CSSProperties}>
-								</span>
+			<div className="">
+				<div className='content-split'>
+					<div className='left'>
+						<div className='line-numbers'>
+							<div className='line'>
+								<div className='content-number'>
+									<span className='plus text-[#00FF99] text-2xl mt-4'>+</span>
+									<span
+										className='counter number text-[54px] text-white'
+										style={{ '--from':6, '--to': 10, '--time': '6s' } as React.CSSProperties}>
+									</span>
+								</div>
+								<p>{t("years")}</p>
 							</div>
-							<p>{t("years")}</p>
-						</div>
-						<div className='line'>
-							<div className='content-number'>
-								<span className='plus text-[#1890FF] text-2xl mt-4'>+</span>
-								<span
-									className='counter number text-[54px] text-white'
-									style={{ '--from': 70, '--to': 100, '--time': '6s' } as React.CSSProperties}>
-								</span>
+							<div className='line'>
+								<div className='content-number'>
+									<span className='plus text-[#1890FF] text-2xl mt-4'>+</span>
+									<span
+										className='counter number text-[54px] text-white'
+										style={{ '--from': 70, '--to': 100, '--time': '6s' } as React.CSSProperties}>
+									</span>
+								</div>
+								<p>{t("customers")}</p>
 							</div>
-							<p>{t("customers")}</p>
-						</div>
-						<div className='line'>
-							<div className='content-number'>
-								<span className='plus text-[#03F2F2] text-2xl mt-4'>+</span>
-								<span
-									className='counter number text-[54px] text-white'
-									style={{ '--from': 50, '--to': 80, '--time': '6s' } as React.CSSProperties}>
-								</span>
+							<div className='line'>
+								<div className='content-number'>
+									<span className='plus text-[#03F2F2] text-2xl mt-4'>+</span>
+									<span
+										className='counter number text-[54px] text-white'
+										style={{ '--from': 50, '--to': 80, '--time': '6s' } as React.CSSProperties}>
+									</span>
+								</div>
+								<p>{t("brands")}</p>
 							</div>
-							<p>{t("brands")}</p>
 						</div>
+					</div>
+
+					<div className='right'>
+						<p><strong>Influid</strong> {t("description")}</p>
 					</div>
 				</div>
 
-				<div className='right'>
-					<p><strong>Influid</strong> {t("description")}</p>
-				</div>
-			</div>
-
-			<div className='content-split-last'>
-				<div className='left'>
-					<span className='number-absolute'>02</span>
-					<h2 className='title text-[64px] font-bold leading-tight text-right'>
-						<span className="text-[#0060FF] block">{t("info-one")}</span>
-						<span className='text-white block'>{t("info-two")}</span>
-						<span className='text-white'>{t("info-three")}</span> <span className='text-[#0060FF]'>.</span>
-					</h2>
-				</div>
-				<div className='right'>
-					<p>{t("solutions")}</p>
-					<a onClick={() => scrollDown('our-projects')} className='scroll-down '>
-						<span>{t("see-our-projects")}</span>
-						<ArrowDown/>
-					</a>
+				<div className='content-split-last'>
+					<div className='left'>
+						<span className='number-absolute'>02</span>
+						<h2 className='title text-[64px] font-bold leading-tight text-right'>
+							<span className="text-[#0060FF] block">{t("info-one")}</span>
+							<span className='text-white block'>{t("info-two")}</span>
+							<span className='text-white'>{t("info-three")}</span> <span className='text-[#0060FF]'>.</span>
+						</h2>
+					</div>
+					<div className='right'>
+						<p>{t("solutions")}</p>
+						<a onClick={() => scrollDown('our-projects')} className='scroll-down '>
+							<span>{t("see-our-projects")}</span>
+							<ArrowDown/>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
