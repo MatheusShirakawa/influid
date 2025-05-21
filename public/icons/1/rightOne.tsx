d0bd0f8afc1ab05af0a7c1f79bf4505d2743b032
MatheusShirakawa <matheus_shirakawa@outlook.com>
2025-05-21 19:28:10 -0300

    :lipstick: changing animation

--- public/icons/1/rightOne.tsx
+++ public/icons/1/rightOne.tsx
@@ -1,7 +1,7 @@
 export default function RightOneIcon() {
 	return(
 		<>
-			<svg width="56" height="109" viewBox="0 0 56 109" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<svg width="55" height="109" viewBox="0 0 55 109" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path d="M55.3262 0.86805C25.283 0.868047 0.928199 24.9397 0.928197 54.6335C0.928194 84.3273 25.283 108.399 55.3262 108.399L55.3262 0.86805Z" fill="#00FF99"/>
 			</svg>
 		</>
